dc0e7264b707f2327fcfe0805512ba0b01ff10e3
Alex Zabashta <zabashta@rain.ifmo.ru>
2013-04-04 00:05:40 +0400

    get args

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(int argc, char **argv)
 {
-	printf("Hello world\n");
+	int bufferSize = 0;
+	void *buffer;	
+	
+	while (*(argv[1]) != '\0')
+	{
+		bufferSize *= 10;
+		bufferSize += *((argv[1])++) - '0';
+	}
+	
+	buffer = malloc(sizeof(char) * bufferSize);
+	
+	read(0, buffer, bufferSize);
+	write(1, buffer, bufferSize);	
 	return 0;
+	
 }
-
