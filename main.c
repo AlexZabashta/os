2b06e6d9ac69f747c5a2e6ce75ccd701c45a2f84
Alex Zabashta <zabashta@rain.ifmo.ru>
2013-04-04 11:48:36 +0400

    add print function

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-	int bufferSize = 0;
-	void *buffer;	
-	
+char *buffer;
+int bufferSize = 0;
+
+int main (int argc, char **argv)
+{	
 	while (*(argv[1]) != '\0')
 	{
 		bufferSize *= 10;
 		bufferSize += *((argv[1])++) - '0';
 	}
 	
-	buffer = malloc(sizeof(char) * bufferSize);
+	buffer = malloc(sizeof(char) * bufferSize);	
 	
-	read(0, buffer, bufferSize);
-	write(1, buffer, bufferSize);	
+	while (1)
+	{
+		int len = read(0, buffer, bufferSize);
+		write(1, buffer, len);
+		putchar('\n');
+		if (len < bufferSize)
+		{
+			break;
+		}
+	}
+	
 	return 0;
 	
+}
+
+void printBuffer (int start, int finish)
+{
+	write(1, buffer + start, finish - start);
+	write(1, buffer + start, finish - start);
 }
