741b5d7280408b42837ec8337fd69a86ca00d1c3
Alex Zabashta <zabashta@rain.ifmo.ru>
2013-04-04 19:09:30 +0400

    s3

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 char *buffer;
-int bufferSize = 0;
+int bufferSize = 5;
 int i;
 int last = 0;
 int isOverFlow = 0;
+int len = 0;
 
 void printBuffer (int start, int finish)
 {
@@ -13,12 +14,18 @@ void printBuffer (int start, int finish)
 	write(1, buffer + start, finish - start);
 }
 
-void searchNewLines(int len)
+void searchNewLines()
 {
 	for (i = last; i < len; i++)
 	{
-		
-	}			
+		if (*(buffer + i) == '\n')
+		{
+			printBuffer(last, i + 1);
+			last = i + 1;
+			isOverFlow = 0;
+		}
+	}
+	
 }
 
 int main (int argc, char **argv)
@@ -33,8 +40,8 @@ int main (int argc, char **argv)
 	
 	while (1)
 	{
-		int len = read(0, buffer + last, bufferSize);
-		
+		len = read(0, buffer + last, bufferSize);
+		searchNewLines();
 		if (isOverFlow) 
 		{
 			for (i = 0; i < len; i++)
@@ -51,6 +58,6 @@ int main (int argc, char **argv)
 		{
 			break;
 		}
-	}		
-	return 0;	
+	}
+	return 0;
 }
