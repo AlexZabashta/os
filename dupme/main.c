89020fd642fd76aad677ca568b1b457616d25a32
AlexZabashta <alex.gmai.alex@gmail.com>
2013-05-21 23:00:41 +0400

    Also some bug fix...

--- dupme/main.c
+++ dupme/main.c
@@ -14,62 +14,66 @@ int isOverFlow = 0;
 int bufferSize = 0;
 
 void printBuffer (int finish) {
-	write(1, buffer + offset, finish - offset);
-	write(1, buffer + offset, finish - offset);
+    write(1, buffer + offset, finish - offset);
+    write(1, buffer + offset, finish - offset);
 }
 
-int main (int argc, char **argv) {	
-	while (*(argv[1]) != '\0')
-	{
-		bufferSize *= 10;
-		bufferSize += *((argv[1])++) - '0';
-	}
-	bufferSize++;
-	buffer = malloc(sizeof(char) * bufferSize);	
-	
-	while (1) {		
-		len = offset;
+int main (int argc, char **argv) {
+    if (argc < 2) {
+        return 1;
+    }
+    while (*(argv[1]) != '\0') {
+        bufferSize *= 10;
+        bufferSize += *((argv[1])++) - '0';
+    }
+    bufferSize++;
+    if ((buffer = malloc(bufferSize)) == NULL) {
+        return 1;
+    }
+    
+    while (1) {        
+        len = offset;
 
-		while (len < bufferSize) {
-			n = read(0, buffer + len, bufferSize - len);			
-			if (n == 0) {
-				isEnd = 1;
-				break;
-			}
-			len += n;
+        while (len < bufferSize) {
+            n = read(0, buffer + len, bufferSize - len);
+            if (n == 0) {
+                isEnd = 1;
+                break;
+            }
+            len += n;
 
-		}
-		offset = 0;
+        }
+        offset = 0;
 
-		for (i = offset; i < len; i++) {
-			if (buffer[i] == '\n') {
-				if (isOverFlow) {
-					if (i + 1 != len) {
-						offset = i + 1;
-						isOverFlow = 0;
-						break;
-					}
-					offset = len;
+        for (i = offset; i < len; i++) {
+            if (buffer[i] == '\n') {
+                if (isOverFlow) {
+                    if (i + 1 != len) {
+                        offset = i + 1;
+                        isOverFlow = 0;
+                        break;
+                    }
+                    offset = len;
 
-				} else {
-					printBuffer(i + 1);
-					offset = i + 1;
-				}
-			}
-		}
-		if (offset != 0 || isEnd) {
-			if (isEnd) {
-				printBuffer(len);
-			}
-			memmove(buffer, buffer + offset, len - offset);			
-			offset = len - offset;
-		} else {
-			isOverFlow = 1;
-		}
-		if (isEnd) {
-			break;
-		}
-	}
-	
-	return 0;
+                } else {
+                    printBuffer(i + 1);
+                    offset = i + 1;
+                }
+            }
+        }
+        if (offset != 0 || isEnd) {
+            if (isEnd) {
+                printBuffer(len);
+            }
+            memmove(buffer, buffer + offset, len - offset);
+            offset = len - offset;
+        } else {
+            isOverFlow = 1;
+        }
+        if (isEnd) {
+            break;
+        }
+    }
+    free(buffer);
+    return 0;
 }
--- filter/filter.c
+++ filter/filter.c
@@ -17,107 +17,108 @@ int offset = 0;
 int bufferSize = 4096;
 
 void printBuffer (int finish) {
-	buffer[finish - 1] = '\0';	
-	ar[ac - 1] = buffer + offset;
-	if(!fork()) {
-		execv(ar[eo], ar + eo);
-	}
-	sleep(1);
+    buffer[finish - 1] = '\0';    
+    ar[ac - 1] = buffer + offset;
+    if(!fork()) {
+        execv(ar[eo], ar + eo);
+    }    
 }
 
 int main (int argc, char **argv) {
-	int opt = 0;
-	char sep = 'q';
-	ar = argv;
-	ac = argc;
-	while((opt = getopt(argc, argv, "nzb:")) != -1) {
-		switch(opt) {
-			case 'n':
-				if (sep == 'q')	{
-					sep = '\n';
-				} else {
-					return 1;
-				}
-				break;
-			case 'z':
-				if (sep == 'q') {
-					sep = '\0';
-				} else {
-					return 1;
-				}
-				break;
-			case 'b':
-				bufferSize = atoi(optarg);
-				break;
-			case '?':
-			default:
-			       return 1;	
-		}	
-	}
-	
-	for(i = 1; i < argc; i++) {
-		if (*(argv[i]) == '-' && *(argv[i] + 1) == 'b' ) {
-			continue;
-		}
-		if (*(argv[i]) == '-' && *(argv[i] + 1) == 'z' ) {
-			continue;
-		}
-		if (*(argv[i]) == '-' && *(argv[i] + 1) == 'n' ) {
-			continue;
-		}
-		if (*(argv[i]) < 58 && *(argv[i]) > 47 ) {
-			continue;
-		}	
-		eo = i - 1;
-		break;
-	}	
-	for (i = eo; i < argc - 1; i++) {
-		argv[i] = argv[i + 1];
-	}
+    int opt = 0;
+    char sep = 'q';
+    ar = argv;
+    ac = argc;
+    while((opt = getopt(argc, argv, "nzb:")) != -1) {
+        switch(opt) {
+            case 'n':
+                if (sep == 'q')    {
+                    sep = '\n';
+                } else {
+                    return 1;
+                }
+                break;
+            case 'z':
+                if (sep == 'q') {
+                    sep = '\0';
+                } else {
+                    return 1;
+                }
+                break;
+            case 'b':
+                bufferSize = atoi(optarg);
+                break;
+            case '?':
+            default:
+                   return 1;    
+        }    
+    }
+    
+    for(i = 1; i < argc; i++) {
+        if (*(argv[i]) == '-' && *(argv[i] + 1) == 'b' ) {
+            continue;
+        }
+        if (*(argv[i]) == '-' && *(argv[i] + 1) == 'z' ) {
+            continue;
+        }
+        if (*(argv[i]) == '-' && *(argv[i] + 1) == 'n' ) {
+            continue;
+        }
+        if (*(argv[i]) < 58 && *(argv[i]) > 47 ) {
+            continue;
+        }    
+        eo = i - 1;
+        break;
+    }    
+    for (i = eo; i < argc - 1; i++) {
+        argv[i] = argv[i + 1];
+    }
 
-	if (sep == 'q') {
-		sep = '\n';
-	}
-	if (bufferSize <= 0) {
-		return 1;
-	}
+    if (sep == 'q') {
+        sep = '\n';
+    }
+    if (bufferSize <= 0) {
+        return 1;
+    }
 
-	bufferSize++;	
-	buffer = malloc(sizeof(char) * bufferSize);	
+    bufferSize++;    
+    if ((buffer = malloc(bufferSize)) == NULL) {
+        return 1;
+    }
 
-	while (1) {		
-		len = offset;
+    while (1) {        
+        len = offset;
 
-		while (len < bufferSize) {
-			n = read(0, buffer + len, bufferSize - len);			
-			if (n == 0) {
-				isEnd = 1;
-				break;
-			}
-			len += n;
+        while (len < bufferSize) {
+            n = read(0, buffer + len, bufferSize - len);            
+            if (n == 0) {
+                isEnd = 1;
+                break;
+            }
+            len += n;
 
-		}
-		offset = 0;
+        }
+        offset = 0;
 
-		for (i = 0; i < len; i++) {
-			if (buffer[i] == sep) {
-				printBuffer(i + 1);
-				offset = i + 1;				
-			}
-		}
-		if (offset != 0 || isEnd) {
-			if (isEnd && offset != len) {
-				printBuffer(len);
-			}
-			memmove(buffer, buffer + offset, len - offset);			
-			offset = len - offset;
-		} else {
-			return 1;
-		}
-		if (isEnd) {
-			break;
-		}
-	}
-	
-	return 0;
+        for (i = 0; i < len; i++) {
+            if (buffer[i] == sep) {
+                printBuffer(i + 1);
+                offset = i + 1;                
+            }
+        }
+        if (offset != 0 || isEnd) {
+            if (isEnd && offset != len) {
+                printBuffer(len);
+            }
+            memmove(buffer, buffer + offset, len - offset);            
+            offset = len - offset;
+        } else {
+            return 1;
+        }
+        if (isEnd) {
+            break;
+        }
+    }
+    
+    return 0;
 }
