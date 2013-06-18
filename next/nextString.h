39a7034fb896c3a5919be42b300b4fd68ff1823e
Alex Zabashta <zabashta@rain.ifmo.ru>
2013-06-19 02:38:47 +0400

    add returns

--- next/nextString.h
+++ next/nextString.h
@@ -12,11 +12,29 @@ struct StreamReader {
 int next(struct StreamReader *s, char sep) {
 	int t = s->l;
 	while(1) {
-		for (s->r = t; s->r < s->len; s->r++) {
+		for(s->r = t; s->r < s->len; s->r++) {
 			if(s->buffer[s->r] == sep) {
-				return 1;
+				return 1; // OK   l:[string + 'sep']:r
 			}
 		}
+		if(s->len == s->size) {
+			if(s->l == 0) {
+				s->len = 0;
+				s->r--;
+				return 2; // buffer overflow 0:[##### + c]:size => c != sep
+			}
+			memmove(s->buffer, s->buffer + s->l, s->len - s->l); // [####labcde] -> [labcde]
+			s->len -= s->l;
+			s->l = 0;
+		}
+		size_t q = read(s->fd, s->buffer + s->len, s->size - s->len);
+		if (q == 0) {
+			s->r--;
+			return 3; // end of stream
+		}
+		t = s->len;
+		s->len += q;
+		
 	}
 	return s->fd;
 }
